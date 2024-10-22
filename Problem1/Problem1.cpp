16261f0a048a99cc6a659a38368ff15cc03bb8a7
Beyah96 <bahsaid96@gmail.com>
2024-10-22 23:23:07 +0200

    Add solution for problem 1

---
+++ Problem1/Problem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector <string> vString{ "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Fiveteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundred", "Thousand", "Million", "Billion", "Trillion"};
+
+int main() {
+	vector <string>::iterator iter;
+	for (string word : vString)
+		vString
+}
