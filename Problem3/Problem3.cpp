57a75d9c41ac14e4b4212a0cc9d58cf0f8dae552
Beyah96 <bahsaid96@gmail.com>
2024-10-22 23:24:20 +0200

    Add solution for problem3

---
+++ Problem3/Problem3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+enum enLeapYearStatus { NotLeapYear = 0, LeapYear = 1 };
+
+short ReadYear(string Message) {
+	short Number;
+	do {
+		cout << Message;
+		cin >> Number;
+	} while (Number < 0);
+	return Number;
+}
+enLeapYearStatus CheckIfLeapYear(short Year) {
+	bool isLeap = false;
+	if ((Year % 400 == 0) || ((Year % 4 == 0) && (Year % 100 != 0)))
+		isLeap = true;
+	return (enLeapYearStatus)isLeap;
+}
+
+
+int main() {
+	short Number = ReadYear("Please enter the year: ");
+	if (CheckIfLeapYear(Number) == enLeapYearStatus::LeapYear)
+		printf("Yes, %d is a leap year", Number);
+	else
+		printf("No, %d is not a leap year", Number);
+	system("pause>0");
+	return 0;
+}
