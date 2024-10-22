73c96b363a4591c3bac61ddb7b3c9ea0f0b84c71
Beyah96 <bahsaid96@gmail.com>
2024-10-22 23:28:32 +0200

    Add solution for the problem 7

---
+++ Problem7/Problem7.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <iomanip> 
+#include <vector>
+
+using namespace std;
+
+vector <string> vDays{ "Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"};
+
+short ReadNumber(string Message) {
+	short Number;
+	do {
+		cout << Message;
+		cin >> Number;
+	} while (Number < 0);
+	return Number;
+}
+
+
+string GetDayName(short DayIndex) {
+	return vDays[DayIndex];
+}
+
+short GetDayOrder(short Day, short Month, short Year) {
+	short a = (14 - Month) / 12;
+	short y = Year - a;
+	short m = Month + 12 * a - 2;
+	return (Day + y + (y / 4) - (y / 100) + (y / 400) + (31 * m / 12)) % 7;
+}
+
+void PrintNameOfDay() {
+	short Day = ReadNumber("Please enter the day : ");
+	short Month = ReadNumber("Please enter the month : ");
+	short Year = ReadNumber("Please enter the year : ");
+	cout << endl;
+	short DayOrder = GetDayOrder(Day, Month, Year);
+	string DayName = GetDayName(DayOrder);
+	printf("Date      : %d / %d / %d\n", Day, Month, Year);
+	cout << "Day Order : " << DayOrder << endl;
+	cout << "Day Name  : " << DayName;
+}
+
+
+int main() {
+	PrintNameOfDay();
+	system("pause>0");
+	return 0;
+}
